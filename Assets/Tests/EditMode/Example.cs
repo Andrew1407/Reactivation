b5e326f92b077bb792cd1f19f5ba7a980c9bcd23
Andrew1407 <endry1407@gmail.com>
2022-03-31 22:39:57 +0300

    initial environment, dependencies & packages (zenject), ci (activation)

---
+++ Assets/Tests/EditMode/Example.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class Example
+{
+    // A Test behaves as an ordinary method
+    [Test]
+    public void ExampleSimplePasses()
+    {
+        Assert.That(true);
+    }
+}
